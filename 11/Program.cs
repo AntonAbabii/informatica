c5efba3338288636bee6a0a9321c417fa3bef8f2
Anton <158073528+AntonAbabii@users.noreply.github.com>
2024-12-10 13:41:06 +0200

    Add files via upload

---
+++ 11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            Array.Sort(people);
+            foreach (var el in people) 
+            {
+                Console.WriteLine(el);
+            }
+            Array.Reverse(people);
+            foreach(var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
